79357f89943668afe9bac1ffa7662947410b5170
Maciej Byczko <byczqq136@gmail.com>
2022-04-24 22:24:38 +0200

    created threaded ghosts
    Signed-off-by: Maciej Byczko <byczqq136@gmail.com>

--- Pacoman/scripts/Main.gd
+++ Pacoman/scripts/Main.gd
@@ -1,60 +1,87 @@
-extends Node2D
+extends Node
 
 var threads = []
-var threads_nr = 2
+var threads_nr = 5
+var coords_for_threads = []
 
 #both inclusive
 var min_x = 6
-var max_x = 20
+var max_x = 23
 var min_y = 0
-var max_y = 14
+var max_y = 16
 
 export(PackedScene) var ghost_scene
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	randomize()
-	var coords_for_threads = []
+	#randomize()
 	for i in range(threads_nr):
 		var coords = get_coordinates()
-		while coords_for_threads.has(coords):
-			coords = get_coordinates()
+		#while coords_for_threads.has(coords):
+		#	coords = get_coordinates()
 		coords_for_threads.append(coords)
 		threads.append(Thread.new())
-		threads[i].start(self, "handle_ghost", coords)
+		#_handle_ghost(coords)
+		threads[i].start(self, "_handle_ghost", coords)
 		
 	#for thread in threads:
 	#	thread.start(self, "handle_ghost")
 	
 	#get_coordinates()
-	
+func get_offset(coords):
+	var mult = 64
+	var add = 32
+	return Vector2(coords[0]*mult+add, coords[1]*mult+add)
 
-func handle_ghost(coords):
-	#var coords = get_coordinates()
-	print(coords)
+func _handle_ghost(coords):
+	
+	#initialize
 	var ghost = ghost_scene.instance()
-	ghost.position = coords
-	print(ghost.position)
-	#call_deferred("add_child", child_node)
+	ghost.position = get_offset(coords)
+	call_deferred("add_child", ghost)
+	ghost.move_and_slide(Vector2(1,1))
+	
+	yield(get_tree().create_timer(1.0), "timeout")
 	#loop
-	pass
+	var destination_mutex = Mutex.new()
+	var destination = ghost.position
+	while(true):
+		if destination == ghost.position:
+			destination_mutex.lock()
+			print(ghost)
+			destination = get_new_destination(ghost.position)
+			destination_mutex.unlock()
+		pass
 
 func get_coordinates():
 	var map = get_node("Walls")
 	var rand_x = randi()%(max_x-min_x) + min_x
 	var rand_y = randi()%(max_y-min_y) + min_y
+	var coords = Vector2(rand_x, rand_y)
 	if map.get_cell(rand_x, rand_y) == -1:
-		return [rand_x, rand_y]
-	else:
-		return get_coordinates()
-	
-	pass
+		if not coords_for_threads.has(coords):
+			return coords
+	return get_coordinates()
 
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+func _exit_tree():
+	for thread in threads:
+		thread.wait_to_finish()
+		
+func get_new_destination(old_coords):
+	var new_coords = get_coordinates()
+	coords_for_threads.erase(old_coords)
+	coords_for_threads.append(new_coords)
+	print("new:" + str(new_coords))
+	return new_coords
+	
+	pass
+	
+	
+	
 
 
-func _on_Menu_start_game():
-	$Player.start($StartPosition.position)
-	pass # Replace with function body.
+	
--- Pacoman/scripts/ghost.gd
+++ Pacoman/scripts/ghost.gd
@@ -4,13 +4,12 @@ extends KinematicBody2D
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-
-
+var speed = 250
+var velocity = Vector2()
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	print("jakiekolwiek")
 	pass # Replace with function body.
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
