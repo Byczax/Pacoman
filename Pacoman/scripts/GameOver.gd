b23f9ed89d0a2b2b131ddcdd7a2f6979613c235b
Bartosz Matysiak <qucker135@gmail.com>
2022-05-28 00:33:24 +0200

    Create Game-Over scene

---
+++ Pacoman/scripts/GameOver.gd
@@ -0,0 +1,23 @@
+extends Control
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$AudioStreamPlayer.play()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_Button_pressed():
+	get_tree().change_scene("res://Menu.tscn")
+
+
+func _on_Timer_timeout():
+	$Button.visible = true
