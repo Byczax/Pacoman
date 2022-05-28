ae82babdf0093a4f8e024518d71e21f597f84bb9
Bartosz Matysiak <qucker135@gmail.com>
2022-05-29 00:33:52 +0200

    Add maze generation

--- Pacoman/scripts/Main.gd
+++ Pacoman/scripts/Main.gd
@@ -2,7 +2,8 @@ extends Node
 
 var threads = [] # array for created threads
 var threads_nr = 5 # thread count
-var coords_for_threads = [Vector2(14, 9)] # critical section with coordinates
+const PACOMAN_INITIAL_POSITION = Vector2(14, 9)
+var coords_for_threads = [PACOMAN_INITIAL_POSITION] # critical section with coordinates
 var destination_mutex = Mutex.new() # associated with coords_for_threads
 #both inclusive
 var min_x = 6
@@ -18,10 +19,13 @@ export(PackedScene) var ghost_scene
 export(PackedScene) var cookie_scene
 export(PackedScene) var teleport_scene
 
+const NR_BLOCKS = 70
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	randomize()
-	generate_points()
+	generate_maze(NR_BLOCKS)
+	generate_points() # generates cookies to gather
 	for i in range(threads_nr):
 		var coords = get_coordinates() # get unique coordinates for each ghost
 		coords_for_threads.append(coords) # add coordinates to critical section
@@ -29,12 +33,57 @@ func _ready():
 		threads[i].start(self, "_handle_ghost", coords) # start thread as ghost
 		$Soundtrack.play()
 
+func generate_maze(nr_blocks):
+	var map = get_node("Walls")
+	var rand_x
+	var rand_y
+	for i in range(nr_blocks):
+		for attempts in range(5):
+			while true:
+				rand_x = randi()%(max_x-min_x) + min_x
+				rand_y = randi()%(max_y-min_y) + min_y
+				if map.get_cell(rand_x, rand_y) == -1 and Vector2(rand_x, rand_y) != PACOMAN_INITIAL_POSITION:
+					break
+			# now rand_x, rand_y coordinates need to be tested
+			var nr_neighbours = 0
+			var neighbours_coords = []
+			for x in [rand_x - 1, rand_x, rand_x + 1]:
+				for y in [rand_y - 1, rand_y, rand_y + 1]:
+					if map.get_cell(x, y) != -1 and !(x == rand_x and y == rand_y):
+						nr_neighbours += 1
+						neighbours_coords.append(Vector2(x, y))
+			if nr_neighbours < 2:
+				map.set_cell(rand_x, rand_y, 0)
+				break
+			elif nr_neighbours >= 4:
+				continue
+			elif nr_neighbours == 2:
+				if abs(neighbours_coords[0].x - neighbours_coords[1].x) == 2 or abs(neighbours_coords[0].y - neighbours_coords[1].y) == 2:
+					continue
+				else:
+					print("Special case 2")
+					map.set_cell(rand_x, rand_y, 0)
+					break
+			else: #nr_neighbours == 3
+				if abs(neighbours_coords[0].x - neighbours_coords[1].x) == 2 or abs(neighbours_coords[0].y - neighbours_coords[1].y) == 2 or abs(neighbours_coords[0].x - neighbours_coords[2].x) == 2 or abs(neighbours_coords[0].y - neighbours_coords[2].y) == 2 or abs(neighbours_coords[2].x - neighbours_coords[1].x) == 2 or abs(neighbours_coords[2].y - neighbours_coords[1].y) == 2:
+					continue
+				else:
+					print("Special case 3")
+					map.set_cell(rand_x, rand_y, 0)
+					break
+			
+	
+	#print("GenerateMaze DUBUG info")
+	#print(rand_x)
+	#print(rand_y)
+	#print(map.get_cell(rand_x, rand_y))
+
 func generate_points():
 	cookies = 0
 	var map = get_node("Walls")
 	for i in range(min_x+1, max_x):
 		for j in range (min_y+1, max_y):
-			if map.get_cell(i,j) == -1 and Vector2(i,j) != Vector2(14, 9):
+			if map.get_cell(i,j) == -1 and Vector2(i,j) != PACOMAN_INITIAL_POSITION:
 				cookies += 1
 				var cookie = cookie_scene.instance()
 				cookie.connect("point_collected", self, "decrement")
@@ -46,7 +95,6 @@ func decrement():
 	cookies -= 1
 	if cookies == 0:
 		var _void = get_tree().change_scene("res://GameWin.tscn")
-	pass
 	
 func get_offset(coords):
 	var mult = 64
@@ -115,7 +163,6 @@ func _exit_tree():
 	exit = true
 	for thread in threads:
 		thread.wait_to_finish()
-#	pass
 	
 func get_new_destination(old_coords):
 	var new_coords = get_coordinates()
