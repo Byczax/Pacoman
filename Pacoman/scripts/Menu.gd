e635ed550b7d029931ff8a0902cc00bbba8d39b9
Maciej Byczko <byczqq136@gmail.com>
2022-05-29 16:01:54 +0200

    add fullscreen functionality
    Signed-off-by: Maciej Byczko <byczqq136@gmail.com>

--- Pacoman/scripts/Menu.gd
+++ Pacoman/scripts/Menu.gd
@@ -3,6 +3,9 @@ extends CanvasLayer
 func _process(_delta):
 	if Input.is_action_pressed("exit"):
 		_on_QuitButton_pressed()
+	if Input.is_action_pressed("fullscreen"):
+		_on_Fullscreen_pressed()
+		OS.delay_msec(300)
 
 
 func _on_Play_button_pressed():
@@ -18,3 +21,8 @@ func _on_QuitButton_pressed():
 func _on_HowToPlayButton_pressed():
 	# warning-ignore:return_value_discarded
 	get_tree().change_scene("res://interfaces/Instruction.tscn")
+
+
+func _on_Fullscreen_pressed():
+	OS.window_fullscreen = !OS.window_fullscreen
+	
